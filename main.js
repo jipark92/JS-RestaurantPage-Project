c04abef186affd97a3a1a1dc477df5f4db025a47
ji <jiyongprk@gmail.com>
2022-03-14 00:12:53 -0400

    wrote javascript to html code

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+// const myHomePageModule =(()=>{
+
+//     const content = document.querySelector('#content')
+//     const homePage = document.createElement("div");
+    
+//     homePage.setAttribute('class', "home-page")
+//     content.appendChild(homePage)
+    
+//     homePage.innerHTML = 
+//     `<nav class="navbar">
+//             <ul></ul>
+//         </nav>
+//     </div>`
+//     ;
+    
+// })();
